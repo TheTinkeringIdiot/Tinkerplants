dea35bffa7ec827d139c6ccedb9e29d5bb97ee47
Tinkering Idiot <admin@tinkeringidiot.com>
2022-02-04 21:35:13 -0500

    add save and load profiles

--- tinkerplants/urls.py
+++ tinkerplants/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('update_display', views.update_display, name='update_display'),
     path('update_imps', views.update_implants, name='update_imps'),
-    path('update_ql', views.update_ql, name='update_ql')
+    path('update_ql', views.update_ql, name='update_ql'),
+    path('save_profile', views.save_profile, name='save_profile'),
+    path('load_profile', views.load_profile, name='load_profile')
 ]
