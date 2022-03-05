804ef5c877ab03862e9efea33ebb5b499d3a137b
Tinkering Idiot <admin@tinkeringidiot.com>
2022-03-04 23:25:07 -0500

    enable collectstatic

--- aobase/settings.py
+++ aobase/settings.py
@@ -88,7 +88,7 @@ if DEVELOPMENT_MODE is True:
             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
-elif len(sys.argv) > 0: # and sys.argv[1] != 'collectstatic':
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
