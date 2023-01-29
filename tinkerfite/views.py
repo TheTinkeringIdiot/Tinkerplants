6cd89ad80f058f2e45368c6d3a90553596273266
Tinkering Idiot <austin.qdm.garrett@gmail.com>
2023-01-29 11:41:44 -0500

    initial framework

--- tinkerfite/views.py
+++ tinkerfite/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    if request.session.get('stats') is None:
+        # request.session['stats'] = initial_nukes()
+        print('Session setup')
+    return render(request, 'tinkernukes/index.html')
