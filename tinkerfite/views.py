743bf0692c465b41c2380df67c371556ae4a7766
Tinkering Idiot <austin.qdm.garrett@gmail.com>
2023-01-30 22:58:40 -0500

    initial setup

--- tinkerfite/views.py
+++ tinkerfite/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, FileResponse, HttpResponseRedirect
+
+from tinkerfite.models import *
+from tinkerfite.utils import *
+from aobase.settings import *
+
+import json, math
 
 # Create your views here.
 def index(request):
     if request.session.get('stats') is None:
-        # request.session['stats'] = initial_nukes()
+        request.session['stats'] = initial_weapons()
         print('Session setup')
-    return render(request, 'tinkernukes/index.html')
+    return render(request, 'tinkerfite/index.html')
+
+def update_display(request):
+    if request.session.get('stats') is None:
+        return JsonResponse({'success': False, 'message': 'Session timed out', 'next': ''})
+
+    weapon_list = get_weapon_list(request.session.get('stats'))
+    return JsonResponse({'success': True, 'stats': request.session.get('stats'), 'weapons' : json.dumps(weapon_list)})
+
+def update_stats(request):
+    if request.method == 'POST':
+        try:
+            if request.session.get('stats') is None:
+                return JsonResponse({'success': False, 'message': 'Session timed out', 'next': ''})
+
+        except Exception as e:
+            #if DEBUG:
+            import traceback
+            traceback.print_exc()
+
+            return JsonResponse({'success': False, 'message': 'If you want to know how it works, just ask'})
+
+def get_weapon_list(stats):
+    return []
