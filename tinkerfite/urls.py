b221001d5543e7e4ab2815afc7d8d08289ac324d
Tinkering Idiot <austin.qdm.garrett@gmail.com>
2023-01-30 22:58:52 -0500

    initial setup

--- tinkerfite/urls.py
+++ tinkerfite/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('update_stats', views.update_stats, name='update_stats'),
+    path('update_display', views.update_display, name='update_display')
 ]
