0997f3d641e189c305d979a011f0b7eb837631cd
Tinkering Idiot <austin.qdm.garrett@gmail.com>
2023-02-08 00:19:27 -0500

    remove default dmg_type

--- tinkerfite/models.py
+++ tinkerfite/models.py
@@ -11,7 +11,7 @@ class Weapon(models.Model):
     dmg_min = models.IntegerField()
     dmg_max = models.IntegerField()
     dmg_crit = models.IntegerField()
-    dmg_type = models.IntegerField(default=0)
+    dmg_type = models.IntegerField()
     clipsize = models.IntegerField()
     props = models.JSONField()
     reqs = models.JSONField()
