c82376d4daf049990ef7efe2e1bfbe0a2d959f8e
Tinkering Idiot <austin.qdm.garrett@gmail.com>
2023-02-11 12:09:33 -0500

    add favicons

--- tinkertools/urls.py
+++ tinkertools/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("robots.txt",TemplateView.as_view(template_name="tinkertools/robots.txt", content_type="text/plain")),
+    path("sitemap.xml",TemplateView.as_view(template_name="tinkertools/sitemap.xml", content_type="text/plain")),
 ]
