4c82542076b36f93f939da8ed914ab37edfbb9cf
Tinkering Idiot <austin.qdm.garrett@gmail.com>
2023-02-15 21:03:48 -0500

    pocketboss lookup

--- tinkerpocket/urls.py
+++ tinkerpocket/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('update_stats', views.update_stats, name='update_stats'),
+    path('boss_info', views.boss_info, name='boss_info'),
     # path('update_display', views.update_display, name='update_display'),
     # path('save_stats', views.save_stats, name='save_stats'),
     # path('restore_stats', views.restore_stats, name='restore_stats')
--- tinkerpocket/views.py
+++ tinkerpocket/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, FileResponse, HttpResponseRedirect
+
+from tinkerpocket.models import *
+# from tinkerpocket.utils import *
+
+
+import json
 
 # Create your views here.
 def index(request):
@@ -6,3 +13,30 @@ def index(request):
         # request.session['stats'] = initial_weapons()
         print('Session setup')
     return render(request, 'tinkerpocket/index.html')
+
+def boss_info(request):
+    if request.method == 'POST':
+        boss_data = json.loads(request.body)
+        name = boss_data['name']
+
+        boss = Pocketboss.objects.filter(name=name)
+        if boss is None or len(boss) != 1:
+            return JsonResponse({'success': False, 'message': 'If you want to know how it works, just ask'})
+
+        boss = boss[0]
+
+        info = {}
+        info['name'] = boss.name
+        info['level'] = boss.level
+        info['mobs'] = boss.mobs
+        info['playfield'] = boss.playfield
+        info['location'] = boss.location
+        info['drops'] = []
+
+        # if name == 'The Night Heart':
+        #     breakpoint()
+
+        for drop in boss.drops.all():
+            info['drops'].append([drop.aoid, drop.name, drop.ql])
+        
+        return JsonResponse({'success': True, 'data': info})
