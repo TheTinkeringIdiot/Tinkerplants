2e6dad3184691a105739de89fb9ca94c4487012d
Tinkering Idiot <austin.qdm.garrett@gmail.com>
2023-02-15 22:46:23 -0500

    add filtering

--- tinkerpocket/urls.py
+++ tinkerpocket/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('boss_info', views.boss_info, name='boss_info'),
+    path('filter', views.filter, name='filter'),
     # path('update_display', views.update_display, name='update_display'),
     # path('save_stats', views.save_stats, name='save_stats'),
     # path('restore_stats', views.restore_stats, name='restore_stats')
--- tinkerpocket/views.py
+++ tinkerpocket/views.py
@@ -40,3 +40,65 @@ def boss_info(request):
             info['drops'].append([drop.aoid, drop.name, drop.ql])
         
         return JsonResponse({'success': True, 'data': info})
+    
+    else:
+        return JsonResponse({'success': False, 'message': 'If you want to know how it works, just ask'})
+
+def filter(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        low_ql = int(data.get('low_ql', 0))
+        high_ql = int(data.get('high_ql', 300))
+        if high_ql < low_ql: 
+            return JsonResponse({'success': False, 'message': 'If you want to know how it works, just ask'})
+
+        families = []
+        if data['artillery']: families.append('Artillery')
+        if data['control']: families.append('Control')
+        if data['extermination']: families.append('Extermination')
+        if data['infantry']: families.append('Infantry')
+        if data['support']: families.append('Support')
+
+        if len(families) <= 0:
+            return JsonResponse({'success': False, 'message': 'If you want to know how it works, just ask'})
+
+        slots = []
+        if data['eye']: slots.append('Eye')
+        if data['head']: slots.append('Head')
+        if data['ear']: slots.append('Ear')
+        if data['right_arm']: slots.append('Right arm')
+        if data['chest']: slots.append('Chest')
+        if data['left_arm']: slots.append('Left arm')
+        if data['right_wrist']: slots.append('Right wrist')
+        if data['waist']: slots.append('Waist')
+        if data['left_wrist']: slots.append('Left wrist')
+        if data['right_hand']: slots.append('Right hand')
+        if data['leg']: slots.append('Legs')
+        if data['left_hand']: slots.append('Left hand')
+        if data['feet']: slots.append('Feet')
+
+        if len(slots) <= 0:
+            symbs = Symbiant.objects.filter(ql__gte=low_ql, ql__lte=high_ql, family__in=families)
+        else:
+            symbs = Symbiant.objects.filter(ql__gte=low_ql, ql__lte=high_ql, family__in=families, slot__in=slots)
+
+        retlist = []
+        for symb in symbs:
+            entry = []
+            entry.append(symb.aoid)
+            entry.append(symb.slot)
+            entry.append(symb.name)
+            entry.append(symb.ql)
+
+            dropped_by = []
+            for boss in symb.dropped_by.all():
+                dropped_by.append(boss.name)
+
+            entry.append(dropped_by)
+            retlist.append(entry)
+
+        return JsonResponse({'success': True, 'data': retlist})
+
+    else:
+        return JsonResponse({'success': False, 'message': 'If you want to know how it works, just ask'})
